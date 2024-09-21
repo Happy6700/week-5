28dd6b561b949666a30195402660a39a7f593fb0
Happy <manncovey@gmail.com>
2024-09-20 19:12:27 -0600

    week 5
    here is an assignment for my programing class

---
+++ Weekfive.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/** weekFive
+ * Madison Covey
+ */
+public class Weekfive {
+public static void main(String[] args) {
+     int [] myNub = {5, 3, 1, 150, 222, 838, 94};
+     Arrays.sort (myNub);
+     for (int i : myNub){
+System.out.println(i);
+
+    }
+}
+    
+}
